3e68155132ff96f28c8cbc5096cdd1a39aaa7a29
Can Kurtur <cankurtur@gmail.com>
2025-06-25 22:49:46 +0200

    init project

---
+++ Vend/Vend/Views/MainView.swift
@@ -0,0 +1,20 @@
+//
+//  MainView.swift
+//  Vend
+//
+//  Created by Can Kurtur on 2025-06-25.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+        }
+    }
+}
+
+#Preview {
+    MainView()
+}
