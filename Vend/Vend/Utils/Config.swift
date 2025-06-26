4d899a8aa7c984266e691cc9d0c00b087925c3b4
Can Kurtur <cankurtur@gmail.com>
2025-06-26 21:05:30 +0200

    create config

---
+++ Vend/Vend/Utils/Config.swift
@@ -0,0 +1,26 @@
+//
+//  Config.swift
+//  Vend
+//
+//  Created by Can Kurtur on 2025-06-26.
+//
+
+import Foundation
+
+class Config {
+    static let shared = Config()
+    
+    var configs: NSDictionary!
+    
+    private init() {
+        let currentConfiguration = Bundle.main.object(forInfoDictionaryKey: "Config")!
+        let path = Bundle.main.path(forResource: "Config", ofType: "plist")!
+        configs = (NSDictionary(contentsOfFile: path)!.object(forKey: currentConfiguration) as! NSDictionary)
+    }
+}
+
+extension Config {
+    var baseUrl: String {
+        return configs.object(forKey: "baseUrl") as! String
+    }
+}
