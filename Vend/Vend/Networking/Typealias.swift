e027587d5850775146078e29d1706bf40c0ee871
Can Kürtür <51170501+cankurtur@users.noreply.github.com>
2025-06-26 21:06:47 +0200

    Feature/networking (#2)
    * create config
    
    * add GlobalNetworking
    
    * create schemas
    
    * create networking

---
+++ Vend/Vend/Networking/Typealias.swift
@@ -0,0 +1,14 @@
+//
+//  Typealias.swift
+//  Vend
+//
+//  Created by Can Kurtur on 2025-06-26.
+//
+
+import GlobalNetworking
+
+typealias APIClientError = GlobalNetworking.APIClientError
+typealias Endpoint = GlobalNetworking.Endpoint
+typealias APIError = GlobalNetworking.APIError
+typealias HTTPHeaders = GlobalNetworking.HTTPHeaders
+typealias HTTPMethod = GlobalNetworking.HTTPMethod
